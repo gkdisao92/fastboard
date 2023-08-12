d97ea4f81cfd3ec5b4ad7d7a44a09cd0a7fecb1b
wonsco <gkdisao92@naver.com>
2023-08-12 10:59:28 +0900

    #6 - 스프링부트 프로젝트 시작
    스프링부트 프로젝트 시작
    
    *스프링웹
    *엑츄에이터
    *데브툴즈
    *롬복

---
+++ src/test/java/com/fastboard/board/FastboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastboard.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
